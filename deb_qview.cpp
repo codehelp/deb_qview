3636ddf4ec474750e5fb95278b4114d8692b58bc
Neil Williams <codehelp@debian.org>
2019-05-15 16:15:47 +0100

    Add licence texts

--- deb_qview.cpp
+++ deb_qview.cpp
@@ -1,9 +1,23 @@
+/*
+    This package is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <QStandardItemModel>
 #include "deb_qview.h"
 #include "ui_deb_qview.h"
 #include "aboutdialog.h"
 #include "ui_aboutdialog.h"
-
 
 deb_qview::deb_qview(QWidget *parent) :
     QMainWindow(parent),
@@ -14,9 +28,12 @@ deb_qview::deb_qview(QWidget *parent) :
     this->status->showMessage(QString("Version: ") + QString(GIT_VERSION));
 
     ui->action_New->setIcon(this->style()->standardIcon(QStyle::SP_FileIcon));
-    ui->action_Open->setIcon(this->style()->standardIcon(QStyle::SP_DialogOpenButton));
-    ui->action_Close->setIcon(this->style()->standardIcon(QStyle::SP_DialogCloseButton));
-    ui->action_Help->setIcon(this->style()->standardIcon(QStyle::SP_DialogHelpButton));
+    ui->action_Open->setIcon(
+                this->style()->standardIcon(QStyle::SP_DialogOpenButton));
+    ui->action_Close->setIcon(
+                this->style()->standardIcon(QStyle::SP_DialogCloseButton));
+    ui->action_Help->setIcon(
+                this->style()->standardIcon(QStyle::SP_DialogHelpButton));
 
 
     this->fileviewer = findChild<QTreeView *>("treeView");
